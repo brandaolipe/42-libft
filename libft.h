e21d858807213a61a6247d00edb1a890d1e6004f
febranda <felipebrandaochaves@gmail.com>
2025-07-21 18:07:16 -0300

    O header com os protótipos das funções: ft_isalpha, ft_isdigit,ft_alnum, ft_isascii, ft_isprint.

---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: febranda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 17:38:31 by febranda          #+#    #+#             */
+/*   Updated: 2025/07/21 17:44:08 by febranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+#endif
