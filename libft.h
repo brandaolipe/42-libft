ee316f299bedea06ba6c17b28c575b4e2f55a38f
febranda <felipebrandaochaves@gmail.com>
2025-07-23 14:13:28 -0300

    .

--- libft.h
+++ libft.h
@@ -3,20 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: febranda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 17:38:31 by febranda          #+#    #+#             */
-/*   Updated: 2025/07/21 17:44:08 by febranda         ###   ########.fr       */
+/*   Updated: 2025/07/23 13:49:04 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stddef.h>
+
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
 
 #endif
