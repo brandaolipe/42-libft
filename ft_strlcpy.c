4ce0938c004602bbac6d05bd38bd1c48e7c6951d
febranda <felipebrandaochaves@gmail.com>
2025-07-26 16:25:03 -0300

    feat: ft_strlcpy()

---
+++ ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: febranda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 19:04:00 by febranda          #+#    #+#             */
+/*   Updated: 2025/07/26 15:17:57 by febranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	size_src;
+
+	size_src = ft_strlen(src);
+	i = 0;
+	if (size == 0)
+		return (size_src);
+	while (src[i] && i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (size_src);
+}
