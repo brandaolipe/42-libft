3ab8a0b54026156c2b8c8f8921b63dce7eb377d4
febranda <felipebrandaochaves@gmail.com>
2025-07-26 16:30:18 -0300

    fix: was not passing a test

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 18:09:40 by febranda          #+#    #+#             */
-/*   Updated: 2025/07/25 18:52:31 by febranda         ###   ########.fr       */
+/*   Updated: 2025/07/26 15:45:45 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while ((*s1 || *s2) && i < n)
 	{
 		if (*s1 != *s2)
-			return (*s1 - *s2);
+			return ((unsigned char)*s1 - (unsigned char)*s2);
 		s1++;
 		s2++;
 		i++;
