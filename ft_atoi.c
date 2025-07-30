7684c7cf350ca349fb649d5813f40ddfef1f5484
Felipe <felipebrandaochaves@gmail.com>
2025-07-30 10:49:08 -0300

    feat: ft_atoi()

---
+++ ft_atoi.c
@@ -0,0 +1,18 @@
+ft_atoi(const char *nptr)
+{
+	int	signal;
+
+	signal = 1;
+	while (*nptr = ' ')
+		nptr++;
+	if (*nptr = '-')
+	{
+		signal *= -1;
+		nptr++;
+	}
+	while (*nptr <= '9' && *nptr >= '0')
+	{
+		number = (number * 10) + (
+	}
+
+}
