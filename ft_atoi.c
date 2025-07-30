d46a3ca4cfd80dea0b5e7ad302d4763faa3ddb38
febranda <felipebrandaochaves@gmail.com>
2025-07-30 17:27:52 -0300

    feat: ft_atoi()

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,18 +1,34 @@
-ft_atoi(const char *nptr)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/30 12:33:25 by febranda          #+#    #+#             */
+/*   Updated: 2025/07/30 13:20:25 by febranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *nptr)
 {
 	int	signal;
+	int	number;
 
 	signal = 1;
-	while (*nptr = ' ')
+	number = 0;
+	while (*nptr == ' ' || (*nptr >= 9 && *nptr <= 13))
 		nptr++;
-	if (*nptr = '-')
+	if (*nptr == '-' || *nptr == '+')
 	{
-		signal *= -1;
+		if (*nptr == '-')
+			signal *= -1;
 		nptr++;
 	}
 	while (*nptr <= '9' && *nptr >= '0')
 	{
-		number = (number * 10) + (
+		number = (number * 10) + (*nptr - 48);
+		nptr++;
 	}
-
+	return (number * signal);
 }
