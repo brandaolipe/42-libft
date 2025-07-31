72088bb990531e5612926a4ef3cc3dafdf349c26
febranda <felipebrandaochaves@gmail.com>
2025-07-31 17:35:23 -0300

    feat: ft_strlcat()

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: febranda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 15:21:52 by febranda          #+#    #+#             */
-/*   Updated: 2025/07/29 19:25:23 by febranda         ###   ########.fr       */
+/*   Updated: 2025/07/30 19:13:19 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
+	size_t	i;
 	size_t	dest_len;
 	size_t	src_len;
 
@@ -23,16 +24,15 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 		dest_len++;
 	while (src[src_len] != '\0')
 		src_len++;
-	if (dest_len < size - 1)
-		return (dest_len + src_len);
-	if (size < dest_len)
+	i = dest_len;
+	if (size <= dest_len)
 		return (size + src_len);
-	while (*src != '\0' && dest_len < size - 1)
+	while (*src != '\0' && i < size - 1)
 	{
-		dest[dest_len] = *src;
+		dest[i] = *src;
 		src++;
-		dest_len++;
+		i++;
 	}
-	*(dest + dest_len) = '\0';
+	*(dest + i) = '\0';
 	return (dest_len + src_len);
 }
